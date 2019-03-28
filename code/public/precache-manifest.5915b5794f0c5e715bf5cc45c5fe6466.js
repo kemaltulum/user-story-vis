dc836969993d340d7a37c64856c2e1114bdcbb17
kemaltulum <tulum.ke@gmail.com>
2019-03-28 16:42:04 +0300

    State after deployment

---
+++ code/public/precache-manifest.5915b5794f0c5e715bf5cc45c5fe6466.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "eb6cee4ceb62f36a7513",
+    "url": "/static/js/main.617d604b.chunk.js"
+  },
+  {
+    "revision": "e8248010ed0ad763434c",
+    "url": "/static/js/2.1c0d1fa8.chunk.js"
+  },
+  {
+    "revision": "eb6cee4ceb62f36a7513",
+    "url": "/static/css/main.6b773e9a.chunk.css"
+  },
+  {
+    "revision": "b82736cf5d5c8c52bdf3e9defff6994e",
+    "url": "/index.html"
+  }
+];
