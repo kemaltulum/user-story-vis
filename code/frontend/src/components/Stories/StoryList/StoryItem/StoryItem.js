72a06f32bce663747d945963cbbe6b8a9c829671
kemaltulum <tulum.ke@gmail.com>
2019-03-30 11:35:50 +0300

    frontend(story-item): Refine the style

--- code/frontend/src/components/Stories/StoryList/StoryItem/StoryItem.js
+++ code/frontend/src/components/Stories/StoryList/StoryItem/StoryItem.js
@@ -3,14 +3,14 @@ import React from 'react';
 import './StoryItem.css';
 
 const storyItem = props => (
-    <li key={props.projectId} className="stories__list-item">
-        <div>
+    <li key={props.projectId} className={"stories__list-item " + (props.isParsed ? "correct" : "incorrect")}>
+        <div className="stories__full-text">
             <p>{props.fullText}</p>
         </div>
-        <div>
-            <p><strong>Actor:</strong> {props.actor}</p>
-            <p><strong>Action:</strong> {props.action}</p>
-            <p><strong>Benefit:</strong> {props.benefit}</p>
+        <div className="stories__details">
+            <p className="stories__actor"><strong>Actor:</strong> {props.actor}</p>
+            <p className="stories__action"><strong>Action:</strong> {props.action}</p>
+            <p className="stories__benefit"><strong>Benefit:</strong> {props.benefit}</p>
         </div>
     </li>
 );
