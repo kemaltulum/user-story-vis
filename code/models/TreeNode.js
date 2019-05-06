657681ee4bd5b3d2491955311c65cace9e967051
kemaltulum <tulum.ke@gmail.com>
2019-05-06 04:46:06 +0300

    backend(model): Add new model TreeNode

---
+++ code/models/TreeNode.js
@@ -0,0 +1,35 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var TreeNodeSchema = new Schema({
+    nodeType: {
+        type: String,
+    },
+
+    name: {
+        type: String,
+        required: true
+    },
+
+    project_id: {
+        type: Schema.Types.ObjectId,
+        ref: 'Project',
+        required: true
+    },
+
+    story_ids: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Story',
+    }],
+
+    children: [{
+        type: Schema.Types.ObjectId,
+        ref: 'TreeNode',
+        required: true
+    }]
+}, { timestamps: true });
+
+
+var TreeNode = mongoose.model('TreeNode', TreeNodeSchema);
+
+module.exports = TreeNode;
